5b7f25760b1bc64fcdad3582d3da1f7c7013e70c
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-14 00:42:48 +0800

    update color total count to red 500

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,7 +16,7 @@
 
 <div class="max-w-6xl mx-auto">
     <div class="flex justify-between items-center mb-4">
-        <h2 class="text-2xl font-bold">Your Goals: <span class="text-red-700">{{ $totalGoals }}</span></h2>
+        <h2 class="text-2xl font-bold">Your Goals: <span class="text-red-500">{{ $totalGoals }}</span></h2>
         <div class="flex flex-row gap-2">
             <a href="{{ route('tasks.create') }}"
                 class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
